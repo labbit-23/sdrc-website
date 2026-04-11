7ff1db5a0c28a7edc6eb1940eb2d6cd94d26cfd5
Pavan Jhamnani <pav@Pavans-MacBook-Air.local>
2026-04-11 14:02:00 +0530

    Added PHP Redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: '/index.php', destination: '/', permanent: true },
+      { source: '/about.php', destination: '/about', permanent: true },
+      { source: '/services.php', destination: '/services', permanent: true },
+      { source: '/packages.php', destination: '/packages', permanent: true },
+      { source: '/packages-prev.php', destination: '/packages', permanent: true },
+      { source: '/packages_prev.php', destination: '/packages', permanent: true },
+      { source: '/contact.php', destination: '/contact', permanent: true },
+      { source: '/accreditation.php', destination: '/accreditation', permanent: true },
+      { source: '/physiotherapy.php', destination: '/physiotherapy', permanent: true },
+      { source: '/privacy-policy.php', destination: '/privacy-policy', permanent: true }
+    ];
+  }
+};
 
 export default nextConfig;
